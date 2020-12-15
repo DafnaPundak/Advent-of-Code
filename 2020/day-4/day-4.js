5119bf04361b069721f1d53c7f37e482dbd38507
Dafna Pundak <dafna.m88@gmail.com>
2020-12-15 11:08:28 +0200

    day-4 part1

---
+++ 2020/day-4/day-4.js
@@ -0,0 +1,56 @@
+const fs = require("fs");
+
+const emptyLineCut = function (array) {
+  let copyArray = [...array];
+  let newArray = [];
+  let indexOfCopy = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] == "") {
+      newArray.push(copyArray.slice(indexOfCopy, i));
+      indexOfCopy = i + 1;
+    }
+  }
+  newArray.push(copyArray.slice(indexOfCopy));
+
+  return newArray;
+};
+
+let data = fs
+  .readFileSync(
+    "/Users/Dafi/Documents/code/Advent of Code/2020/day-4/input.txt",
+    "utf8"
+  )
+  .toString()
+  .split("\n")
+  .map((s) => s.replace(/\r$/, ""));
+
+let data2 = emptyLineCut(data);
+
+const countValid = function (array) {
+  numValid = 0;
+  for (let passport of data2) {
+    let newPassport = passport
+      .join()
+      .split(":")
+      .join()
+      .split(",")
+      .join()
+      .split(/(\s+)/)
+      .filter((e) => e.trim().length > 0)
+      .join()
+      .split(",");
+
+    newPassport.includes("eyr") &&
+    newPassport.includes("byr") &&
+    newPassport.includes("iyr") &&
+    newPassport.includes("hgt") &&
+    newPassport.includes("hcl") &&
+    newPassport.includes("ecl") &&
+    newPassport.includes("pid")
+      ? numValid++
+      : "null";
+  }
+  console.log(numValid);
+};
+
+countValid(data2);
